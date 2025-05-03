2a6e2725d7cf0999594198fe180df6f5423054fb
Marek <mwojdyla0@gmail.com>
2025-05-03 13:25:22 +0200

    update techniczny

--- mysite/urls.py
+++ mysite/urls.py
@@ -37,4 +37,4 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, documen_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
